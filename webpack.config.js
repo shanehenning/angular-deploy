86fca8240afcc89c817e3e90678d8b0abbe25851
Shane Henning <shanerhenning@gmail.com>
2016-09-01 16:06:02 -0700

    duncan details

--- webpack.config.js
+++ webpack.config.js
@@ -57,10 +57,7 @@ module.exports = {
       {
         test: /\.js$/,
         loader: 'babel',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015'],
-        },
+        exclude: /node_modules/
       },
       {
         test: /\.html$/,
@@ -78,13 +75,13 @@ module.exports = {
         test: /\.woff.*/,
         loader: 'file?name=fonts/[name].[ext]',
       },
-      { 
+      {
         test: /\.[ot]tf.*/,
-        loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]' 
+        loader: 'url?limit=10000&mimetype=application/octet-stream&name=fonts/[name].[ext]'
       },
       {
-        test: /\.eot.*/, 
-        loader: 'url?limit=10000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]' 
+        test: /\.eot.*/,
+        loader: 'url?limit=10000&mimetype=application/vnd.ms-fontobject&name=fonts/[name].[ext]'
       }
     ],
   },
